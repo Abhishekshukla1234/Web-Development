890096e69df9c129bc8272d8db41ce74c95f27d3
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-04-30 12:52:12 +0530

    commit all css file
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/1.Introduction to javascript/script.js
@@ -0,0 +1,3 @@
+let c=24;
+let d=45;
+console.log("c+d is",c+d);
---
+++ learnJS/2.Data type/script.js
@@ -0,0 +1,23 @@
+//let a=32;
+
+//let a='23'
+
+let a='0';
+//a=a+1;
+
+console.log(a);
+a=Boolean(a);
+// a='name';
+
+
+// a=false;
+
+console.log('the value of a is',a);
+console.log('the data type of a',typeof(a));
+
+// let b
+// let b=null;
+let b = 'hello'
+
+b=Number(b)
+console.log('b is',b)
