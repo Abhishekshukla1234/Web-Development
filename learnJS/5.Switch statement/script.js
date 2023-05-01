2571e600f2caa01adbc7aa76eb2c189b750a0bee
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-05-02 01:36:38 +0530

    commit
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/5.Switch statement/script.js
@@ -0,0 +1,18 @@
+const grade="A";
+
+switch(grade)
+{
+    case "A":
+        console.log("A -> very good");
+        break;
+        case "B":
+            console.log("B -> good , keep learning");
+            break;
+            case "C":
+                console.log("C -> improvement");
+                break;
+                case "D":
+                    console.log("D -> not good");
+                    break;
+                                
+}
