ebfc42a52e2a3ab98acd09d277f7de87bdefb494
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-05-03 08:38:29 +0530

    Arrays in js
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/7.Arrays in js/script.js
@@ -0,0 +1,27 @@
+//const words = ['Apply','Banana','Cherry',12,
+//{
+//    name:'Ahuj'
+//},
+//function hello(){
+//    console.log('hello')
+//}
+//];
+//const newwords=words;
+//newwords[1]='kiwi'
+//console.log(words)
+//console.log(newwords)
+
+
+const nums=[1,2,3,4,0,6,78];
+
+//Array method
+
+//console.log(nums.indexOf(4))
+//console.log(nums.includes(4))
+//nums.push(17)
+//nums.unshift("Abhishek")
+//nums.pop()
+//nums.shift()
+//nums.sort()
+const subArray = nums.slice(3,6)
+console.log(subArray)
