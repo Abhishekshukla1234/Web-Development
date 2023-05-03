28b0b82bd7e02c0710d623b773358217d2945666
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-05-03 07:38:21 +0530

    loops in javascript
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/6.loops/script.js
@@ -0,0 +1,31 @@
+//for loop
+//for(let i=0; i<=5; i++){
+  //  console.log('Abhishek', i);
+//}
+
+
+//while loop
+//let step=0;
+//while(step<10){
+//    console.log('step',step)
+  //  step++;
+//}
+
+
+//do while loop
+//let step =6;
+//do{
+  //  console.log('step',step);
+    //step += 1;
+//}while(step<4)
+
+//while loop
+let step=0;
+while(step<10){
+  step++;
+    if(step ==3){
+    continue;
+    }
+    console.log('step',step)
+    
+}
