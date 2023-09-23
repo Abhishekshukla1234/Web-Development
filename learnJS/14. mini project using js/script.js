5120a98b2e11d6e7d6b5148b46c6d5f25f9ebf95
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-09-23 19:31:49 +0530

    mini project for using html css and js
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/14. mini project using js/script.js
@@ -0,0 +1,12 @@
+const accordians = document.querySelectorAll('.accordian');
+
+accordians.forEach(accordian => {
+    const icon = accordian.querySelector(".icon")
+    const answer = accordian.querySelector(".answer")
+
+    accordian.addEventListener("click", () => {
+        icon.classList.toggle("active")
+        answer.classList.toggle("active")
+
+       })
+    })
