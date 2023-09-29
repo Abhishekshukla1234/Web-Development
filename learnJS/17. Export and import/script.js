412ea99cb3eb68451160b360caeb34603fba08d4
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2023-09-29 15:08:34 +0530

    Error handling
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ learnJS/17. Export and import/script.js
@@ -0,0 +1,6 @@
+// import{multiply as mul} from './utility.js'
+import * as utility from './utility.js'
+
+const result = utility.mul.add(2, 3, 4);
+
+console.log(result);
---
+++ learnJS/17. Export and import/utility.js
@@ -0,0 +1,14 @@
+ function multiply(a, b, c){
+  return a*b*c;
+}
+
+
+function addition (a,b,c){
+    return a+b+c;
+}
+
+export{
+    multiply as mul,
+    addition as add
+
+}
